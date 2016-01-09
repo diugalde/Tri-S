ac5f77952dd71e9de27282f7caa2d6f7f16b4894
Luis <luis-ex@live.com>
2016-01-09 01:06:46 -0600

    Finishing all unit tests for first iteration - Includes 	- Tests for FormValidator 	- Tests for NotificationManager

--- webService/src/test/java/cr/ac/siua/tec/utils/NotificationManagerTest.java
+++ webService/src/test/java/cr/ac/siua/tec/utils/NotificationManagerTest.java
@@ -2,17 +2,25 @@ package cr.ac.siua.tec.utils;
 
 import org.junit.Test;
 
+import java.util.Map;
+
+import static cr.ac.siua.tec.utils.NotificationManager.getInvalidFormMsg;
+import static cr.ac.siua.tec.utils.NotificationManager.getValidFormMsg;
 import static org.junit.Assert.*;
 
 public class NotificationManagerTest {
 
     @Test
     public void testGetValidFormMsg() throws Exception {
-
+        Map<String, String> result = getValidFormMsg();
+        assertEquals(result.get("type"), "success");
+        assertEquals(result.get("msg"), "El formulario ha sido enviado exitosamente. Se envió un correo a su cuenta.");
     }
 
     @Test
     public void testGetInvalidFormMsg() throws Exception {
-
+        Map<String, String> result = getInvalidFormMsg();
+        assertEquals(result.get("type"), "error");
+        assertEquals(result.get("msg"), "Error al procesar el formulario. Revise los campos llenados.");
     }
 }
