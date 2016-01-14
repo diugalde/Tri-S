8df1bca427c4a5d2abdc3e21064f7c86127207ae
diugalde <dua.0695@gmail.com>
2016-01-14 05:26:22 -0600

    Adding enrolled courses dynamic feature to the form

--- webService/src/main/resources/static/mobile/js/form_loader.js
+++ webService/src/main/resources/static/mobile/js/form_loader.js
@@ -3,6 +3,26 @@ var ready = function() {
         var htmlFile = $(this).attr('id');
         $(this).load("forms/" + htmlFile + ".html");
     });
+
+    $("body").on("tap", "#addcourse-btn", function(ev) {
+    	ev.preventDefault();
+		courseCode = $("#dynamic-course-code").val();
+		courseName = $("#dynamic-course-name").val();
+		courseNumber = $("#dynamic-course-number").val();
+
+		if(courseCode.trim() != "" && courseName.trim() != "" && courseNumber.trim() != "") {
+			row = " " + courseCode + " - " + courseName + " - " + courseNumber + "\n";
+			$("#dynamic-courses").val($("#dynamic-courses").val()+row); 
+			$("#dynamic-course-code").val("");
+			$("#dynamic-course-name").val("");
+			$("#dynamic-course-number").val("");
+		}
+    });
+
+    $("body").on("tap", "#resetcourses-btn", function(ev) {
+    	ev.preventDefault();
+		$("#dynamic-courses").val("");
+    });
 }
 
 $(document).on('page:load', ready);
--- webService/src/main/resources/static/web/js/loadForms.js
+++ webService/src/main/resources/static/web/js/loadForms.js
@@ -22,6 +22,26 @@ var ready = function() {
 				});
 
 				$(this).find(".toggle-bootstrap").bootstrapToggle();
+
+				$(this).find("#addcourse-btn").click(function(ev) {
+					ev.preventDefault();
+					courseCode = $("#dynamic-course-code").val();
+					courseName = $("#dynamic-course-name").val();
+					courseNumber = $("#dynamic-course-number").val();
+
+					if(courseCode.trim() != "" && courseName.trim() != "" && courseNumber.trim() != "") {
+						row = " " + courseCode + " - " + courseName + " - " + courseNumber + "\n";
+						$("#dynamic-courses").val($("#dynamic-courses").val()+row); 
+						$("#dynamic-course-code").val("");
+						$("#dynamic-course-name").val("");
+						$("#dynamic-course-number").val("");
+					}
+				});
+
+				$(this).find("#resetcourses-btn").click(function(ev) {
+					ev.preventDefault();
+					$("#dynamic-courses").val("");
+				});
 			});
 		});	
 	}
